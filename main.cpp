5a614d5654851c8560908761ae98918ef857bec7
mattrogrady <mattrogrady@gmail.com>
2020-11-24 10:44:24 -0800

    Added main.cpp stub

---
+++ main.cpp
@@ -0,0 +1,14 @@
+/*
+This file will serve as the main file that we will run to execute our code. It should include appropriate classes and be simple, allowing classes and function calls to do most of the work for it. For now, we will just have it print 'Hello World' as a stub until we are ready with more code.
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	cout << "Hello World" << endl;
+	
+	return 0;
+}
+
